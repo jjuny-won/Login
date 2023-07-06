5c9bfd582776279ed63ad8b5407a6b62b6022323
jjuny-won <jjuny040627@gmai.com>
2023-07-06 23:26:24 +0900

    alert 에 로그아웃 추가

--- src/main/java/com/work1/login/repository/MemberRepository.java
+++ src/main/java/com/work1/login/repository/MemberRepository.java
@@ -6,15 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.util.Optional;
 
 //어떤 Entity 받을 것이냐, id 의 타입
 //반드시 Entity 객체로 넘어줘야함
 public interface MemberRepository extends JpaRepository <MemberEntity, Long>{
-    //이메일로 회원정보 조회
+    //아이디로 회원정보 조회
     //알아서 쿼리가 만들어짐
-   // (select * from user where email=?)
+   // (select * from user where id=?)
     Optional<MemberEntity> findByMemberId(String Id);
 
     @Modifying
